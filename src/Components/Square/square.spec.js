2c918883d88ab7bd97f88c6636e605e51f1c2e08
hadijahkyampeire <hadijah315@gmail.com>
2019-02-27 12:34:57 +0300

    delete unnecessary files

--- src/Components/Square/square.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Square from './square';
-import { shallow } from 'enzyme';
-
-describe('Square tests', () => {
-  it('renders without crashing', () => {
-    shallow(<Square />);
-  });
-});
